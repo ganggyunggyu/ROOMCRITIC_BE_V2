58be518f1217e2254a6afdba8b1520b56c1d4768
강경규 <qwzx16@naver.com>
2024-05-24 22:08:04 +0900

    feat: content c/u/d에 따른 genrescore 계산

--- src/genre-score/genre-score.service.ts
+++ src/genre-score/genre-score.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { GenreScore } from './schema/genre-scores.schema';
+import { getGenreName } from 'src/user/constant/GENRE_SCORES';
 
 @Injectable()
 export class GenreScoreService {
@@ -15,11 +16,55 @@ export class GenreScoreService {
       .find(
         {
           userId: userId,
+          score: { $ne: 0 },
         },
         { genreName: 1, count: 1, score: 1 },
       )
       .sort({ score: -1 });
 
     return result;
+  }
+
+  async addGenreScore(genreIds: number[], grade: number, userId: string) {
+    for (let i = 0; i < genreIds.length; i++) {
+      const genreId = genreIds[i];
+
+      await this.genreScoreModel.updateOne(
+        { userId: userId, genreId: genreId },
+        {
+          $inc: { score: grade, count: 1 },
+          $setOnInsert: { genreName: getGenreName(genreId) },
+        },
+        { upsert: true },
+      );
+    }
+  }
+  async subtractGenreScore(genreIds: number[], grade: number, userId: string) {
+    for (let i = 0; i < genreIds.length; i++) {
+      const genreId = genreIds[i];
+
+      await this.genreScoreModel.updateOne(
+        { userId: userId, genreId: genreId },
+        {
+          $inc: { score: -grade, count: -1 },
+          $setOnInsert: { genreName: getGenreName(genreId) },
+        },
+        { upsert: true },
+      );
+    }
+  }
+  async updateGenreScore(genreIds: number[], grade: number, userId: string) {
+    for (let i = 0; i < genreIds.length; i++) {
+      const genreId = genreIds[i];
+
+      await this.genreScoreModel.updateOne(
+        { userId: userId, genreId: genreId },
+        {
+          $inc: { score: grade },
+          $setOnInsert: { genreName: getGenreName(genreId) },
+        },
+        { upsert: true },
+      );
+    }
   }
 }
