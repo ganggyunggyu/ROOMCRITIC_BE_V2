1570246231ce9ffcdd70d54d32efc626877220e0
강경규 <qwzx16@naver.com>
2024-05-24 22:07:17 +0900

    test: jwt auth test

--- src/auth/guard/jwt-auth.guard.ts
+++ src/auth/guard/jwt-auth.guard.ts
@@ -2,4 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor() {
+    super();
+  }
+  // async getAuthenticateOptions(context: ExecutionContext) {
+  //   console.log(context.switchToHttp());
+  // }
+}
