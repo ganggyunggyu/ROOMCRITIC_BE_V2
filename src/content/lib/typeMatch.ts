8ceea1501c996de9ad625edef09fc893fed7b978
강경규 <qwzx16@naver.com>
2024-05-30 00:04:34 +0900

    feat: util 함수 생성

---
+++ src/content/lib/typeMatch.ts
@@ -0,0 +1,5 @@
+export const typeMatch = (type: 'tv' | 'movie' | 'all') => {
+  if (type === 'tv') return { $match: { contentType: 'tv' } };
+  if (type === 'movie') return { $match: { contentType: 'movie' } };
+  if (type === 'all') return { $match: {} };
+};
