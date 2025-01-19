4a854853f15225a246c14f814901a7e8519d3bdd
ganggyunggyu <qwzx16@naver.com>
2025-01-19 13:17:49 +0900

    chore: development https remove

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { swaggerConfig } from './shared/swagger/init.swagger';
-import * as fs from 'fs';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync(process.env.SSL_KEY_PATH),
-    cert: fs.readFileSync(process.env.SSL_CERT_PATH),
-  };
-  // const app = await NestFactory.create(AppModule, { httpsOptions });
   const app = await NestFactory.create(AppModule);
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
 
   SwaggerModule.setup('api', app, document);
   app.enableCors({
-    // origin: 'https://localhost:5173',
     origin: '*',
     credentials: true,
     exposedHeaders: ['Authorization'],
