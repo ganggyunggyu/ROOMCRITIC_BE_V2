4bff3ddea056b4a244f35c2dd7302656cdd12072
ganggyunggyu <qwzx16@naver.com>
2025-02-01 23:34:56 +0900

    test: user-test-add

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { LoginRequestDTO } from './dto/user.dto';
 
 describe('UsersController', () => {
   let controller: UserController;
@@ -13,6 +14,12 @@ describe('UsersController', () => {
   });
 
   it('should be defined', () => {
+    const LOGIN_MOCK: LoginRequestDTO = {
+      email: 'test@test.test',
+      password: 'test',
+    };
+    controller.login(LOGIN_MOCK);
+
     expect(controller).toBeDefined();
   });
 });
