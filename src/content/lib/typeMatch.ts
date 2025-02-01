c082d90bcaf70dae3dc311f141ee2f61568729c7
ganggyunggyu <qwzx16@naver.com>
2025-02-01 23:35:07 +0900

    chore: default return value add

--- src/content/lib/typeMatch.ts
+++ src/content/lib/typeMatch.ts
@@ -2,4 +2,5 @@ export const typeMatch = (type: 'tv' | 'movie' | 'all') => {
   if (type === 'tv') return { $match: { contentType: 'tv' } };
   if (type === 'movie') return { $match: { contentType: 'movie' } };
   if (type === 'all') return { $match: {} };
+  return { $match: {} };
 };
